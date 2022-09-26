b0fa4c534ea1624af2f730e2c1ea340e73c48bb5
Egor Ivanov <cheshi.mantu@pm.me>
2022-09-26 17:46:20 +0300

    clean up

--- src/test/java/tests/ReqResInApiTests.java
+++ src/test/java/tests/ReqResInApiTests.java
@@ -1,25 +1,11 @@
 package tests;
 
-import helpers.AttachmentsHelper;
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
-import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
-import io.restassured.config.HttpClientConfig;
-import io.restassured.config.RestAssuredConfig;
-import io.restassured.http.Method;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import org.apache.http.params.CoreConnectionPNames;
 import org.junit.jupiter.api.*;
 
-import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class ReqResInApiTests extends TestBase {
